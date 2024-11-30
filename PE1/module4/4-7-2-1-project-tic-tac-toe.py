bcbddebed0c27862e47b9052faa4886298bb3273
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-11-30 20:41:59 +0200

    refactor!: start implementation from scratch
    BREAKING CHANGE: removing existing code to implement the exercise requirements properly

--- PE1/module4/4-7-2-1-project-tic-tac-toe.py
+++ PE1/module4/4-7-2-1-project-tic-tac-toe.py
@@ -1,18 +1,24 @@
-
-board = [
-    ["1", "2", "3"],  
-    ["4", "5", "6"],  
-    ["7", "8", "9"]   
-]
-
-line = "+-------+-------+-------+"
-empty_line = "|       |       |       |"
-number_line = f"|   {board[0][0]}   |   {board[0][1]}   |   {board[0][2]}   |"
+from random import randrange
 
 def display_board(board):
-    for row in range(3):
-        print(line)
-        print(empty_line)
-        print(f"|   {board[row][0]}   |   {board[row][1]}   |   {board[row][2]}   |")
-        print(empty_line)
-    print(line)
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+
+
+def enter_move(board):
+    # The function accepts the board's current status, asks the user about their move, 
+    # checks the input, and updates the board according to the user's decision.
+
+
+def make_list_of_free_fields(board):
+    # The function browses the board and builds a list of all the free squares; 
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
+
+def victory_for(board, sign):
+    # The function analyzes the board's status in order to check if 
+    # the player using 'O's or 'X's has won the game
+
+
+def draw_move(board):
+    # The function draws the computer's move and updates the board.
