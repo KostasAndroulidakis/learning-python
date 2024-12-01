7491bf31c2bb8f6231af68ea4c75aa666f16192b
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-01 21:18:59 +0200

    refactor(module3): convert lesson 3.2.1.5 to python script
    - Convert all examples from Jupyter notebook to Python script
    - Add explanatory comments for each range() variation
    - Keep all examples from the lesson in a single file

---
+++ openedg/PE1/module3/3_2_1_5_loops_in_python.py
@@ -0,0 +1,11 @@
+# Basic for loop with range(stop)
+for i in range(10):
+    print("The value of i is currently", i)
+
+# For loop with range(start, stop)
+for i in range(2, 8):
+    print("The value of i is currently", i)
+
+# For loop with range(start, stop, step)
+for i in range(2, 8, 3):
+    print("The value of i is currently", i)
