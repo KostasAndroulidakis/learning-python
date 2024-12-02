da8ca228513976a0c6a656ef4f727a433b2071a6
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-02 20:22:37 +0200

    feat(PE2/module2): add mysplit function that mimics split() method

--- curriculum/PE2/module2/2_3_1_18_lab.py
+++ curriculum/PE2/module2/2_3_1_18_lab.py
@@ -1,8 +1,24 @@
 def mysplit(strng):
-    #
-    # put your code here
-    #
-
+    # prepare a string
+    word = ""
+    # prepare a list to add the words
+    words = []
+    # remove whitespaces from the start and end of the string
+    strng = strng.strip()
+    # in case of empty string
+    if strng == "":
+        # return an empty list
+        return words
+    else:
+        for char in strng:
+            if not char.isspace():
+                # add last word after the end of the string
+                word = word + char          
+            if char.isspace():
+                words.append(word)
+                word = ""
+    words.append(word)
+    return words
 
 print(mysplit("To be or not to be, that is the question"))
 print(mysplit("To be or not to be,that is the question"))
