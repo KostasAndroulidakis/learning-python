4fc39e3580926cadeeb5f21323de9bf3ee8d2665
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-04 00:42:07 +0200

    refactor(curriculum/PE1/module2): move basic operators to 2.3.1.1 py file

---
+++ curriculum/PE1/module2/2_3_1_1.py
@@ -0,0 +1,14 @@
+# Addition
+print(2 + 2)
+# Subtraction
+print(2 - 2)
+# Multiplication
+print(2 * 2)
+# Division (returns float)
+print(2 / 2)
+# Integer division
+print(2 // 2)
+# Modulo (returns remainder)
+print(2 % 2)
+# Exponentiation (power)
+print(2 ** 2)
