d9e9266214fe157dc427c87317ddc21c31423a92
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-04 15:32:04 +0200

    complete(curriculum/PE1/module2): complete 2.4.1.10 LAB: Operators and expressions

--- curriculum/PE1/module2/2_4_1_10_lab.py
+++ curriculum/PE1/module2/2_4_1_10_lab.py
@@ -1,4 +1,14 @@
-x =  # hardcode your test data here
+x = 0
 x = float(x)
-# write your code here
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
+
+x = 1
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
+
+x = -1
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x - 1
 print("y =", y)
