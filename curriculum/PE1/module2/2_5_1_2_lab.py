4d53f2410fdc35841b3cfd3ba5630e75f17a0dd7
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-04 17:19:47 +0200

    feat(curriculum/PE1/module2): complete 2.5.1.2 LAB: Comments

---
+++ curriculum/PE1/module2/2_5_1_2_lab.py
@@ -0,0 +1,6 @@
+hours = 2   # Number of hours
+seconds = 3600   # Number of seconds in 1 hour
+
+print("Hours: ", hours)   # printing the number of hours
+print("Seconds in Hours: ", hours * seconds)   # printing the number of seconds in a given number of hours
+print("Goodbye")   # printing goodbye
