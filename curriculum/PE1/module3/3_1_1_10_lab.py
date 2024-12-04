97717625123ba68fbc30b75daef115e4c01c2a60
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-04 21:46:58 +0200

    feat(curriculum/PE1/module) complete 3.1.1.10 LAB: Comparison operators and conditional execution

---
+++ curriculum/PE1/module3/3_1_1_10_lab.py
@@ -0,0 +1,8 @@
+word = input("Enter a word: ")
+
+if word == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+if word == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print(f"Spathiphyllum! Not {word}!")
