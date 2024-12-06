f905ffd797c6594c9248d0fa04ebf1b5ac56938e
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-06 23:08:43 +0200

    feat(curriculum/PE1/module3): complete 3.1.1.12 LAB: Essentials of the if-elif-else statement

--- curriculum/PE1/module3/3_1_1_12_lab.py
+++ curriculum/PE1/module3/3_1_1_12_lab.py
@@ -1,5 +1,15 @@
 year = int(input("Enter a year: "))
 
-#
-# Write your code here.
-#	
+leap = 366
+common = 365
+
+if year < 1582:
+    print("Not within the Gregorian calendar period")
+elif year % 4 != 0:
+    print("Common year")
+elif year % 100 != 0:
+    print("Leap year")
+elif year % 400 != 0:
+    print("Common year")
+else:
+    print("Leap year")
