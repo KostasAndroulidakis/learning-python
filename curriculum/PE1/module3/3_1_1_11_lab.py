3ec57f5a5eb1ef6a9de7cc5d6f1d4dc6c505be22
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-06 22:45:28 +0200

    feat(curriculum/PE1/module3): complete 3.1.1.11 LAB: Essentials of the if-else statement

--- curriculum/PE1/module3/3_1_1_11_lab.py
+++ curriculum/PE1/module3/3_1_1_11_lab.py
@@ -1,8 +1,17 @@
 income = float(input("Enter the annual income: "))
 
-#
-# Write your code here.
-#
+LIMIT = 85528
+TAX_RELIEF = 556.02
+LOW_TAX = 0.18
+HIGH_TAX = 0.32
+
+if income <= LIMIT:
+    if income*LOW_TAX < TAX_RELIEF:
+        tax = 0
+    else:
+        tax = (income*LOW_TAX)-TAX_RELIEF
+else:
+    tax = 14839.02+((income-LIMIT)*HIGH_TAX)
 
 tax = round(tax, 0)
 print("The tax is:", tax, "thalers")
