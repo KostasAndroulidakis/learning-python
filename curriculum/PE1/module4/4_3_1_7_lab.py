7472df405bb5827678f651a14c137b35b55a136f
KostasAndroulidakis <kostasandroulidakis.pro@gmail.com>
2024-12-10 20:46:21 +0200

    feat(curriculum/PE1/module4): complete 4.3.1.7 LAB: How many days: writing and using your own functions

--- curriculum/PE1/module4/4_3_1_7_lab.py
+++ curriculum/PE1/module4/4_3_1_7_lab.py
@@ -1,12 +1,26 @@
 def is_year_leap(year):
-#
-# Your code from LAB 4.3.1.6.
-#
+      if year % 400 == 0:
+        return True
+      elif year % 100 == 0 and year % 400 != 0:
+        return False
+      elif year % 4 == 0:
+        return True
+      return False
 
+# Create function
 def days_in_month(year, month):
-#
-# Write your new code here.
-#
+	# Create a list with all months
+	months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+	# Check if month is valid
+	if month < 1 or month > 12:
+		# If not valid
+		return None
+	# Get the days for each month
+	days = months[month - 1]
+	# Check for leap year's February
+	if month == 2 and is_year_leap(year):
+		days = 29
+	return days
 
 test_years = [1900, 2000, 2016, 1987]
 test_months = [2, 2, 1, 11]
