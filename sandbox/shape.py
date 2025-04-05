bd40675e45ca8c78570b9c5a32f260a95027494f
Stratos Papadoudis <stratos.papadoudis@helvia.io>
2025-04-05 18:18:18 +0000

    Draft of an early shape tree

--- sandbox/shape.py
+++ sandbox/shape.py
@@ -1,25 +1,29 @@
 from __future__ import annotations
 
 
+import math
 from typing import Self
 
 
-class Rectangle:
+class Parallelogram:
 
 	def __init__(self,
 		width: float,
 		height: float,
+		angle: float,
 	):
 		self.width = width
 		self.height = height
+		self.angle = angle
 
 	def __repr__(self) -> str:
-		return f"{self.__class__.__name__}({self.width}, {self.height})"
+		return self.__class__.__name__ + str(vars(self))
 
 	def __add__(self, other: Self) -> Self:
 		return self.__class__(
 			self.width + other.width,
 			self.height + other.height,
+			(self.angle + other.angle) / 2,
 		)
 
 
@@ -29,7 +33,38 @@ class Rectangle:
 
 	@property
 	def area(self) -> float:
+		return self.width * self.height * math.cos(self.angle)
+
+
+class Rectangle(Parallelogram):
+
+	def __init__(self,
+		width: float,
+		height: float,
+	):
+		super().__init__(
+			width,
+			height,
+			math.pi / 2,
+		)
+
+
+	@property
+	def area(self) -> float:
 		return self.width * self.height
+
+
+class Rombus(Parallelogram):
+
+	def __init__(self,
+		side: float,
+		angle: float,
+	):
+		super().__init__(
+			side,
+			side,
+			angle,
+		)
 
 
 class Square(Rectangle):
@@ -37,29 +72,7 @@ class Square(Rectangle):
 	def __init__(self,
 		side: float,
 	):
-		self.width = side
-		self.height = side
-
-
-class DummySquare:
-
-	def __init__(self,
-		side: float
-	):
-		self.width = side
-		self.height = side
-
-	def __repr__(self) -> str:
-		return f"Square({self.width})"
-
-	def __add__(self, other: Self) -> Self:
-		return self.__class__(self.width + other.height)
-
-
-	@property
-	def perimeter(self) -> float:
-		return 2 * (self.width + self.height)
-
-	@property
-	def area(self) -> float:
-		return self.width * self.height
+		super().__init__(
+			side,
+			side,
+		)
